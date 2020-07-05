28d6d0eb252f946e1af2dd0b72e842426fd74056
Andy Woerner <andy.woerner@icloud.com>
2020-07-05 23:02:03 +0200

    refactor: EventBus for communication added

--- main.js
+++ main.js
@@ -1,23 +1,53 @@
+const EventBus = new Vue();
+
+
 const InputComponent = {
     template: `
             <input 
                 type="text"
                 class="form-control"
                 :placeholder="placeholder"
+                v-model="note" 
+
+                @keyup.enter="submitNote"
             >
         `,
-        props: ["placeholder"]
-        
+    props: ["placeholder"],
+    // data muss innerhalb der component eine Funktion sein. Gibt ein Objekt mit den Daten zurück
+    data() {
+        return {
+            note: ""
+        }
+    },
+    methods: {
+        submitNote() {
+            EventBus.$emit("new-note", {
+                note: this.note,
+                timestamp: new Date().toLocaleString()
+            })
+            this.note = ""; //nachdem die Notiz gesendet wurde, wird das Feld wieder leer gemacht
+        }
+    }
+
 }
 
 new Vue({
-    el:"#app",
-    components:{
+    el: "#app",
+    components: {
         "input-component": InputComponent
     },
     data: {
         notes: [],
         timestamps: [],
         placeholder: "Gib hier deine neue Notiz an"
+    },
+    methods: {
+        storeNote(event) {
+            this.notes.push(event.note)
+            this.timestamps.push(event.timestamp)
+        }
+    },
+    created(){
+        EventBus.$on("new-note", event => this.storeNote(event))
     }
 })
