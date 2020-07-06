8a9734d6ce72d70a309643be06b193a1c5c3cc15
Andy Woerner <andy.woerner@icloud.com>
2020-07-06 15:32:54 +0200

    feat: add "edit note" feature

--- main.js
+++ main.js
@@ -1,43 +1,49 @@
 //VUEX
 // 4 Komponenten eines VUEX Stores
 const state = {
-    notes:[
-        {
-            note: "Das ist eine Notiz",
-            timestamp: new Date().toLocaleString()
-        }
-    ]
+    notes: [{
+        note: "Das ist eine Notiz",
+        timestamp: new Date().toLocaleString()
+    }]
 }
 // getters sind wie die computed properties im store
 const getters = {
-   getNotes(){
-    return state.notes;
-   },
-   getNoteCount(){
-    return state.notes.length
+    getNotes() {
+        return state.notes;
+    },
+    getNoteCount() {
+        return state.notes.length
 
-   }
+    }
 }
 // Funktionen die unseren State ändern können
 // Funktionen der Mutations werden GROSS geschrieben
 const mutations = {
-    STORE_NOTE (state, payload) { //payload sind die Daten die gespeichert werden sollen. Gängige Benennung.
+    STORE_NOTE(state, payload) { //payload sind die Daten die gespeichert werden sollen. Gängige Benennung.
         state.notes.push(payload)
     },
-    REMOVE_NOTE(state, index){
+    REMOVE_NOTE(state, index) {
         state.notes.splice(index, 1)
+    },
+    UPDATE_NOTE(state, payload) {
+        state.notes[payload.index] = payload.payload
     }
 }
 // Actions
 const actions = {
-    storeNote(context, payload) { 
+    storeNote(context, payload) {
         //context ist ähnlich wie state. Aber über den context können wir getters kontaktieren und state aufrufen
         //function commit -> damit wird ein mutation aufgerufen   
         context.commit("STORE_NOTE", payload)
     },
-    removeNote (context, timestamp){
-        const index = context.state.notes.findIndex( note => note.timestamp === timestamp)
+    removeNote(context, timestamp) {
+        const index = context.state.notes.findIndex(note => note.timestamp === timestamp)
         context.commit("REMOVE_NOTE", index)
+    },
+    updateNote(context, payload) {
+        const index = context.state.notes.findIndex(note => note.timestamp === payload.timestamp)
+        payload.timestamp = new Date().toLocaleString()
+        context.commit("UPDATE_NOTE", { index, payload })
     }
 }
 
@@ -57,7 +63,7 @@ const NoteCountComponent = {
         <div>Anzahl der Notizen : <strong> {{ noteCount }}</strong></div>
     `,
     computed: {
-        noteCount(){
+        noteCount() {
             return this.$store.getters.getNoteCount
         }
     }
@@ -85,10 +91,10 @@ const InputComponent = {
     },
     methods: {
         submitNote() {
-           const newNote = {
-               note: this.note,
-               timestamp: new Date().toLocaleString()
-           }
+            const newNote = {
+                note: this.note,
+                timestamp: new Date().toLocaleString()
+            }
             this.$store.dispatch("storeNote", newNote)
             this.note = ""; //nachdem die Notiz gesendet wurde, wird das Feld wieder leer gemacht
         }
@@ -106,10 +112,12 @@ new Vue({
         "note-count-component": NoteCountComponent
     },
     data: {
-        placeholder: "Gib hier deine neue Notiz an"
-    }, 
+        placeholder: "Gib hier deine neue Notiz an",
+        activeEditNote: null ,
+        activeEditNoteContent: ""
+    },
     computed: {
-        notes () {
+        notes() {
             return this.$store.getters.getNotes
         }
     },
@@ -118,8 +126,20 @@ new Vue({
             this.notes.push(event.note)
             this.timestamps.push(event.timestamp)
         },
-        deleteNote(noteTimestamp){
-            this.$store.dispatch("removeNote",noteTimestamp)
+        deleteNote(noteTimestamp) {
+            this.$store.dispatch("removeNote", noteTimestamp)
+        },
+        editNote(note) {
+            this.activeEditNote = note
+            this.activeEditNoteContent = this.activeEditNote.note
+        },
+        updateNote() {
+            const newNote = {
+                note: this.activeEditNoteContent,
+                timestamp: this.activeEditNote.timestamp
+            }
+            this.$store.dispatch("updateNote", newNote)
+            this.activeEditNote = null
         }
     }
 })
